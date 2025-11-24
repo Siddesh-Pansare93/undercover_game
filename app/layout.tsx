2da51d8f5f470508202421d6def24c84bf8ec862
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-11-24 12:00:00 +0530

    Add SVG favicon and generate icon scripts; update manifest and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className="antialiased">
         <div className="min-h-screen bg-base">
